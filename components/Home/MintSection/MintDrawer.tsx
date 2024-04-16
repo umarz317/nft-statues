a5ba82dd164847e172ae67f451fa7c2fc419a3c4
Ali Emad <aliemademad02@gmail.com>
2024-04-16 02:50:00 +0200

    NewPopup add new mint pop-up

--- components/Home/MintSection/MintDrawer.tsx
+++ components/Home/MintSection/MintDrawer.tsx
@@ -50,8 +50,8 @@ export default function MintDrawer() {
       }}
       className="fixed left-0 top-0 z-[1000] w-full h-full mint-drawer-wrapper-popup"
     >
-      <div className="mint-drawer-popup-right absolute z-10 h-full max-h-[500px] w-full max-w-[800px] mx-auto left-0 right-0 top-0 bottom-0 my-auto p-6 lg:p-8">
-        <div className="relative mint-drawer-popup-content w-full h-full bg-[#0F0F0F] rounded-[4rem] flex flex-col justify-between items-center p-6 lg:p-6">
+      <div className="mint-drawer-popup-right absolute z-10 h-full max-h-[718px] w-full max-w-[589px] mx-auto left-0 right-0 top-0 bottom-0 my-auto p-6 lg:p-8">
+        <div className="relative mint-drawer-popup-content w-full h-full bg-[url(/static/images/mint-popupBG.png)] bg-center bg-no-repeat bg-contain flex flex-col justify-center items-center p-6 lg:p-6 overflow-hidden">
           {/* <button
             onClick={() => setOpenMint(false)}
             className="absolute right-6 top-6 w-10 h-10 bg-[#1F1F1F]/70 rounded-full z-10 flex items-center justify-center text-white"
@@ -70,6 +70,102 @@ export default function MintDrawer() {
               />
             </svg>
           </button> */}
+
+          <div className="w-full grid grid-cols-2 grid-rows-2 gap-6 px-6">
+            <div className="flex flex-col justify-center items-center rounded-[27px] overflow-hidden">
+              <Image
+                src="/static/images/mint-popup-image.png"
+                width={186}
+                height={183}
+                alt="image"
+                className="w-[186px]"
+              />
+            </div>
+            <div className="flex flex-col justify-center items-center rounded-[27px] overflow-hidden">
+              <Image
+                src="/static/images/mint-popup-image.png"
+                width={186}
+                height={183}
+                alt="image"
+                className="w-[186px]"
+              />
+            </div>
+            <div className="flex flex-col justify-center items-center rounded-[27px] overflow-hidden">
+              <Image
+                src="/static/images/mint-popup-image.png"
+                width={186}
+                height={183}
+                alt="image"
+                className="w-[186px]"
+              />
+            </div>
+            <div className="flex flex-col justify-center items-center rounded-[27px] overflow-hidden">
+              <Image
+                src="/static/images/mint-popup-image.png"
+                width={186}
+                height={183}
+                alt="image"
+                className="w-[186px]"
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex items-center justify-between px-6 mt-[44px] mb-[26px]">
+            <button className="w-[52px] h-[52px] bg-[url(/static/images/mint-popup-arrow.png)] bg-center bg-no-repeat bg-cover hover:scale-105 transition-transform duration-300 ease-out"></button>
+            <div className="flex flex-col items-center justify-center gap-3">
+              <span className="block text-white text-2xl lg:text-[57px] font-medium tracking-tighter">
+                {title}
+              </span>
+              <span className="block text-white opacity-[.4] text-lg lg:text-[28px] font-normal tracking-tighter text-center">
+                {price} ETH Price
+              </span>
+            </div>
+            <button className="w-[52px] h-[52px] bg-[url(/static/images/mint-popup-arrow.png)] bg-center bg-no-repeat bg-cover transform rotate-180 hover:scale-105 transition-transform duration-300 ease-out"></button>
+          </div>
+
+          <button
+            onClick={() => setOpenMint(false)}
+            className="w-[80%] lg:w-fit group hover:scale-105 transition-transform duration-300 ease-out flex flex-row items-center justify-center gap-2 text-black font-bold text-base lg:text-xl tracking-tighter bg-[#ff3600] rounded-full lg:px-10 py-2"
+          >
+            <span>Mint</span>
+            <span className="block w-3 group-hover:translate-x-1 transition-transform duration-300 ease-out">
+              <svg
+                width="100%"
+                viewBox="0 0 15 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7.81607 0.945313L13.0742 6.20345L7.81607 11.4616M0.30443 6.20345L12.6986 6.20346"
+                  stroke="black"
+                  strokeWidth="1.50233"
+                />
+              </svg>
+            </span>
+          </button>
+        </div>
+      </div>
+      {/* old popup */}
+      {/* <div className="mint-drawer-popup-right absolute z-10 h-full max-h-[500px] w-full max-w-[800px] mx-auto left-0 right-0 top-0 bottom-0 my-auto p-6 lg:p-8">
+        <div className="relative mint-drawer-popup-content w-full h-full bg-[#0F0F0F] rounded-[4rem] flex flex-col justify-between items-center p-6 lg:p-6">
+          <button
+            onClick={() => setOpenMint(false)}
+            className="absolute right-6 top-6 w-10 h-10 bg-[#1F1F1F]/70 rounded-full z-10 flex items-center justify-center text-white"
+          >
+            <svg
+              width="30%"
+              viewBox="0 0 21 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M1.76478 0L10.5 8.73522L19.2352 0L21 1.76478L12.2648 10.5L21 19.2352L19.2352 21L10.5 12.2648L1.76478 21L0 19.2352L8.73522 10.5L0 1.76478L1.76478 0Z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
           <Image
             src="/static/images/white-jimbo.svg"
             width={55}
@@ -134,7 +230,7 @@ export default function MintDrawer() {
             </span>
           </button>
         </div>
-      </div>
+      </div> */}
       <div
         onClick={() => setOpenMint(false)}
         className="absolute inset-0 bg-black/30 backdrop-blur-2xl opacity-0 mint-drawer-popup-bg z-0"

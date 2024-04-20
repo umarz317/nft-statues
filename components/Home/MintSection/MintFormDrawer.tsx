f6941acc21846028c39ac204c41d77a127b53885
Ali Emad <aliemademad02@gmail.com>
2024-04-20 18:38:46 +0200

    FixPopups create all inputs in mint form pop-up

--- components/Home/MintSection/MintFormDrawer.tsx
+++ components/Home/MintSection/MintFormDrawer.tsx
@@ -1,34 +1,27 @@
 import { useMintItemDrawer } from "@/hooks/useMintItemDrawer";
-import useIsomorphicLayoutEffect from "@/hooks/useIsomorphicLayout";
 import { gsap } from "@/lib/gsap";
 import { useEffect } from "react";
-import Image from "next/image";
 import { useLenis } from "@studio-freight/react-lenis";
-import { urlFor } from "@/lib/sanity/sanityClient";
+import { useForm } from "react-hook-form";
+
+const InputsData = [
+  { name: "name", label: "Name", type: "text" },
+  { name: "shipping-address", label: "Shipping Address", type: "text" },
+  { name: "contact-phone-number", label: "Contact Phone Number", type: "tel" },
+  { name: "email", label: "Email", type: "email" },
+  { name: "twitter", label: "Twitter/X", type: "text" },
+];
 
 export default function MintFormDrawer() {
-  const {
-    title,
-    height,
-    price,
-    material,
-    weight,
-    setTitle,
-    setHeight,
-    setPrice,
-    setMaterial,
-    setWeight,
-    isOpenMint,
-    setOpenMint,
-    statuesValue,
-    isOpenMintForm,
-    setOpenMintForm,
-  } = useMintItemDrawer();
-  console.log("isOpenMintForm:", isOpenMintForm);
+  const { isOpenMintForm, setOpenMintForm } = useMintItemDrawer();
   const lenis = useLenis(() => {});
 
-  console.log("statuesValue:", statuesValue);
-  console.log("title:", title);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   useEffect(() => {
     const html = document.querySelector("html");
     const drawer = document.querySelector(`.mint-form-drawer-wrapper-popup`);
@@ -43,11 +36,11 @@ export default function MintFormDrawer() {
       html?.classList.add("locked");
       lenis?.stop();
       tl.to(bg, { opacity: 1 });
-      tl.to(drawerRight, { x: "0%" }, 0);
+      tl.to(drawerRight, { opacity: 1 }, 0);
     } else {
       html?.classList.remove("locked");
       lenis?.start();
-      tl.to(drawerRight, { x: "100%" });
+      tl.to(drawerRight, { opacity: 0 });
       tl.to(
         bg,
         {
@@ -62,6 +55,11 @@ export default function MintFormDrawer() {
     };
   }, [isOpenMintForm, lenis]);
 
+  const onSubmit = (data: any) => {
+    console.log("data:", data);
+    setOpenMintForm(false);
+  };
+
   return (
     <div
       style={{
@@ -69,7 +67,7 @@ export default function MintFormDrawer() {
       }}
       className="fixed left-0 top-0 z-[1000] w-full h-full mint-form-drawer-wrapper-popup"
     >
-      <div className="mint-form-drawer-popup-right absolute z-10 h-full max-h-[600px] sm:max-h-[718px] w-full max-w-[589px] right-0 top-0 bottom-0 my-auto p-6 sm:p-8">
+      <div className="mint-form-drawer-popup-right absolute z-10 h-screen w-screen top-0 left-0 p-6 sm:p-8">
         <div className="relative mint-form-drawer-popup-content rounded-3xl w-full h-full bg-[#0B0B0B] bg-center bg-no-repeat bg-contain flex flex-col justify-center items-center p-2 sm:p-6">
           <div className="angles z-10">
             <svg
@@ -173,7 +171,7 @@ export default function MintFormDrawer() {
               />
             </svg>
           </div>
-          {/* <button
+          <button
             onClick={() => setOpenMintForm(false)}
             className="absolute right-2 top-2 w-9 sm:w-10 h-9 sm:h-10 bg-[#191919] rounded-full z-10 flex items-center justify-center text-[#717171] hover:scale-105 transition-transform duration-300 ease-out"
           >
@@ -190,80 +188,51 @@ export default function MintFormDrawer() {
                 fill="currentColor"
               />
             </svg>
-          </button> */}
-
-          <div className="w-full grid grid-cols-2 grid-rows-2 gap-6 px-6">
-            {statuesValue.map((statue) => (
-              <button
-                key={statue._id}
-                onClick={() => {
-                  setTitle(statue.title);
-                  setHeight(statue.height);
-                  setPrice(statue.price);
-                  setMaterial(statue.material);
-                  setWeight(statue.weight);
-                }}
-                className={`flex flex-col justify-center items-center rounded-[27px] overflow-hidden ${
-                  statue.title === title ? "opacity-100" : "opacity-20"
-                } duration-300`}
-              >
-                <Image
-                  src={urlFor(statue.image).url()}
-                  width={186}
-                  height={160}
-                  alt={statue.title}
-                  className="w-[150px] sm:w-[186px] h-[120px] sm:h-[160px]"
-                />
-              </button>
-            ))}
-          </div>
-
-          <div className="w-full flex items-center justify-center px-6 mt-[30px] mb-[20px]">
-            {/* <button className="md:w-[52px] w-[30px] md:h-[52px] h-[30px] bg-[url(/static/images/mint-popup-arrow.png)] bg-center bg-no-repeat bg-cover hover:scale-105 transition-transform duration-300 ease-out"></button> */}
-            <div className="flex flex-col items-center justify-center gap-2">
-              {!title && !price ? (
-                <>
-                  <span className="block text-white text-2xl lg:text-4xl font-medium tracking-tighter">
-                    Please select statue
-                  </span>
-                  <span className="block invisible text-white opacity-[.4] text-lg lg:text-xl font-normal tracking-tighter text-center">
-                    {price} ETH Price
-                  </span>
-                </>
-              ) : (
-                <>
-                  <span className="block text-white text-2xl lg:text-4xl font-medium tracking-tighter">
-                    {title}
-                  </span>
-                  <span className="block text-white opacity-[.4] text-lg lg:text-xl font-normal tracking-tighter text-center">
-                    {price} ETH Price
-                  </span>
-                </>
-              )}
-            </div>
-            {/* <button className="md:w-[52px] w-[30px] md:h-[52px] h-[30px] bg-[url(/static/images/mint-popup-arrow.png)] bg-center bg-no-repeat bg-cover transform rotate-180 hover:scale-105 transition-transform duration-300 ease-out"></button> */}
-          </div>
-
-          <button
-            onClick={() => setOpenMintForm(false)}
-            className="w-[80%] mt-6 lg:w-fit group hover:scale-105 transition-transform duration-300 ease-out flex flex-row items-center justify-center gap-2 text-black font-medium text-base lg:text-xl tracking-tighter bg-[#ff3600] rounded-full lg:px-10 py-2"
-          >
-            <span>Mint</span>
-            <span className="block w-3 group-hover:translate-x-1 transition-transform duration-300 ease-out">
-              <svg
-                width="100%"
-                viewBox="0 0 15 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7.81607 0.945313L13.0742 6.20345L7.81607 11.4616M0.30443 6.20345L12.6986 6.20346"
-                  stroke="black"
-                  strokeWidth="1.50233"
-                />
-              </svg>
-            </span>
           </button>
+
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="w-full grid grid-cols-2 grid-rows-3 gap-6 px-6"
+          >
+            {InputsData.map((input) => (
+              <div key={input.name}>
+                <label htmlFor={input.name} className="block mb-1 text-white">
+                  {input.label}:
+                </label>
+                <input
+                  {...register(input.name)}
+                  type={input.type}
+                  id={input.name}
+                  name={input.name}
+                  className="w-full border border-gray-300 bg-transparent text-white rounded-full px-5 py-2"
+                />
+              </div>
+            ))}
+
+            <div>
+              <label className="block mb-1 text-white">Forge:</label>
+              <button
+                type="submit"
+                className="w-full group hover:scale-105 transition-transform duration-300 ease-out flex flex-row items-center justify-center gap-2 text-black font-medium text-base lg:text-xl tracking-tighter bg-[#ff3600] rounded-full lg:px-10 py-2"
+              >
+                <span>Forge</span>
+                <span className="block w-3 group-hover:translate-x-1 transition-transform duration-300 ease-out">
+                  <svg
+                    width="100%"
+                    viewBox="0 0 15 12"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M7.81607 0.945313L13.0742 6.20345L7.81607 11.4616M0.30443 6.20345L12.6986 6.20346"
+                      stroke="black"
+                      strokeWidth="1.50233"
+                    />
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </form>
         </div>
       </div>
 
